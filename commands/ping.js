7834479964269347ceebec9130383c188e4fa2a4
BadSpencer <46595495+BadSpencer@users.noreply.github.com>
2020-01-30 00:12:10 +0100

    Ajout desktop.ini

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,6 @@
-const { Command } = require('discord-akairo');
+const {
+    Command
+} = require('discord-akairo');
 
 class PingCommand extends Command {
     constructor() {
@@ -8,15 +10,11 @@ class PingCommand extends Command {
     }
 
     exec(message) {
-
-        /*
-        return message.util.reply('Pong!').then(sent => {
+        return message.util.send('Pong!').then(sent => {
             const timeDiff = (sent.editedAt || sent.createdAt) - (message.editedAt || message.createdAt);
-            const text = `🔂\u2000**RTT**: ${timeDiff} ms\n💟\u2000**Heartbeat**: ${Math.round(this.client.ping)} ms`;
-            return message.util.reply(`Pong!\n${text}`);
+            const text = `🔂\u2000**API Discord**: ${timeDiff} ms\n💟\u2000**Ping**: ${Math.round(this.client.ping)} ms`;
+            return message.util.send(`Pong!\n${text}`);
         });
-        */
-        return message.reply('Pong!');
     }
 }
 
