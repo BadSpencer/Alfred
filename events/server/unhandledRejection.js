4ab737338e4f83f56208fd542369d778673d096a
BadSpencer <46595495+BadSpencer@users.noreply.github.com>
2020-02-02 10:31:10 +0100

    Gestion erreurs

---
+++ events/server/unhandledRejection.js
@@ -0,0 +1,20 @@
+const { Listener } = require('discord-akairo');
+const chalk = require('chalk');
+const moment = require('moment');
+
+class UnhandledRejectionListener extends Listener {
+    constructor() {
+        super('unhandledRejection', {
+            eventName: 'unhandledRejection',
+            emitter: 'process'
+        });
+    }
+
+    exec(error) {
+        let timestamp = `${moment(new Date()).format("DD-MM-YY HH:mm:ss")}`;
+        let log = `${timestamp} | ${chalk.bgRed.bold(`ERROR`)} | ${error}`
+        console.log(log);
+    }
+}
+
+module.exports = UnhandledRejectionListener;
