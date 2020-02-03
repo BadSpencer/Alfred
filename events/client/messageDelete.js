83b74c6d63ec355d9be7c3db77f4efef920b4e59
BadSpencer <46595495+BadSpencer@users.noreply.github.com>
2020-02-03 16:23:38 +0100

    Ajout events

---
+++ events/client/messageDelete.js
@@ -0,0 +1,27 @@
+const {
+    Listener
+} = require('discord-akairo');
+const chalk = require('chalk');
+const moment = require('moment');
+
+
+class MessageDeleteListener extends Listener {
+    constructor() {
+        super('messageDelete', {
+            emitter: 'client',
+            eventName: 'messageDelete'
+        });
+    }
+
+    exec(message) {
+        let timestamp = `${moment(new Date()).format("DD-MM-YY HH:mm:ss")}`;
+
+        let deleted = `Le message ${message.id} à été supprimé dans le salon <#${message.channel.id}>` 
+
+        console.log(`${timestamp} | ${deleted}`);
+
+
+    }
+}
+
+module.exports = MessageDeleteListener;
