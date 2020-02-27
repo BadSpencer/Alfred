c933222d0208c913ee3dd8aa6b196929bf5a2e12
BadSpencer <46595495+BadSpencer@users.noreply.github.com>
2020-02-27 17:59:10 +0100

    Gain xp avec réactions

--- events/client/messageReactionAdd.js
+++ events/client/messageReactionAdd.js
@@ -8,7 +8,7 @@ const {
     questionMessage
 } = require('../../utils/messages');
 
-const textes = require('../../utils/textes');
+const emojis = require('../../utils/emojis');
 
 class MessageReactionAddListener extends Listener {
     constructor() {
@@ -73,18 +73,40 @@ class MessageReactionAddListener extends Listener {
                 const gameRole = guild.roles.get(game.roleID);
                 if (gameRole) {
                     if (member.roles.has(gameRole.id)) {
-                        client.games.quitConfirmation(client, messageReaction,  game, member);
+                        client.games.quitConfirmation(client, messageReaction, game, member);
                     } else {
                         member.addRole(gameRole);
                         successMessage(client.textes.get(`GAMES_JOIN_SUCCESS`, game.name), messageReaction.message.channel);
                     }
-                } 
+                }
             } else {
                 // Jeu non trouvé avec emoji
             }
         }
 
 
+        if (messageReaction.message.member !== member) {
+            client.db.userdataAddXP(client, member, 5, `Réaction à un message`);
+
+
+            if (emojis.positive.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 20, `Réaction "positive" reçue`);
+            if (emojis.positiveHand.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 20, `Réaction "positiveHand" reçue`);
+            if (emojis.positivePlus.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 30, `Réaction "positivePlus" reçue`);
+            if (emojis.neutral.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 10, `Réaction "neutral" reçue`);
+            if (emojis.neutralHand.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 10, `Réaction "neutralHand" reçue`);
+            if (emojis.bad.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 5, `Réaction "bad" reçue`);
+            if (emojis.badHand.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 5, `Réaction "badHand" reçue`);
+            if (emojis.sad.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 10, `Réaction "sad" reçue`);
+            if (emojis.love.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 50, `Réaction "love" reçue`);
+            if (emojis.sweet.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 25, `Réaction "sweet" reçue`);
+            if (emojis.drink.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 15, `Réaction "drink" reçue`);
+            if (emojis.flower.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 20, `Réaction "flower" reçue`);
+            if (emojis.event.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 10, `Réaction "event" reçue`);
+            if (emojis.medal.includes(messageReaction.emoji.name)) client.db.userdataAddXP(client, member, 100, `Réaction "medal" reçue`);
+
+
+
+        }
 
 
 
