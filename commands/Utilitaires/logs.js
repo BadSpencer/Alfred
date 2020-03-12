870d529e66e9131f55d6db3dc9b45659f392ff9b
BadSpencer <46595495+BadSpencer@users.noreply.github.com>
2020-03-12 17:50:36 +0100

    Commande !logs

---
+++ commands/Utilitaires/logs.js
@@ -0,0 +1,44 @@
+const {
+    Command
+} = require('discord-akairo');
+const { Permissions } = require('discord.js');
+
+class TestCommand extends Command {
+    constructor() {
+        super('logs', {
+            aliases: ['logs'],
+            userPermissions: [Permissions.FLAGS.MANAGE_MESSAGES],
+            category: 'Utilitaires',
+            cooldown: 30000,
+            ratelimit: 1,
+            description: 'Récupérer les logs',
+        });
+    }
+
+    async exec(message, args) {
+        let client = this.client;
+        const guild = client.guilds.get(client.config.guildID);
+        const settings = await client.db.getSettings(client);
+
+
+        message.channel.send("Logs", {files: ["/root/.pm2/logs/index-out.log"]});
+        //message.channel.send("Logs", {files: ["C:\\Users\\Bad\\Documents\\AlfredProd\\data\\enmap.sqlite"]});
+
+
+        /*
+        switch (args.action) {
+            case 'dl':
+
+            break;
+            case 'debug':
+
+            break;
+        }
+        */
+
+
+    }
+}
+
+
+module.exports = TestCommand;
