41c31b7eebc92287e820d32b12576020f745a598
BadSpencer <46595495+BadSpencer@users.noreply.github.com>
2020-03-15 06:20:36 +0100

    code clean

--- utils/logs.js
+++ utils/logs.js
@@ -1,13 +1,17 @@
 module.exports = (client) => {
 
-    client.log = async (msg, type = "log") => {
-        const settings = await client.db.getSettings(client);
-        client.logger.log(msg, type);
-        if (settings.logChannelID && settings.logToChan == "true") {
-          if (type == "debug" && settings.debug == "false") return;
-          client.channels.get(settings.logChannelID).send(`[${type.toUpperCase()}] ${msg}`).catch(console.error);
-        }
-      };
+  client.log = async (msg, type = "log") => {
+    const settings = await client.db.getSettings(client);
+    if (!settings) {
+      console.log(msg);
+    } else {
+      client.logger.log(msg, type);
+      if (settings.logChannelID && settings.logToChan == "true") {
+        if (type == "debug" && settings.debug == "false") return;
+        client.channels.get(settings.logChannelID).send(`[${type.toUpperCase()}] ${msg}`).catch(console.error);
+      }
+    }
+  }
+};
 
 
-}
