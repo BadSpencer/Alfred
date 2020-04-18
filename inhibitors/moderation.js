9796c517315876a79b76f9b5b45311292f12d60c
BadSpencer <46595495+BadSpencer@users.noreply.github.com>
2020-04-18 10:49:06 +0200

    Code clean

--- inhibitors/moderation.js
+++ inhibitors/moderation.js
@@ -13,11 +13,12 @@ class ModerationCheck extends Inhibitor {
         const guild = client.guilds.get(client.config.guildID);
         let member = guild.members.get(message.author.id);
 
-        const roleEve = guild.roles.find(r => r.name == "@everyone");
-        const roleMem = guild.roles.find(r => r.name == message.settings.memberRole);
-        const roleMod = guild.roles.find(r => r.name == message.settings.modRole);
-        const roleAdm = guild.roles.find(r => r.name == message.settings.adminRole);
+        //const roleEve = guild.roles.find(r => { return r.name == "@everyone" });
+        //const roleMem = guild.roles.find(r => { return r.name == message.settings.memberRole });
+        const roleMod = guild.roles.find(r => { return r.name == message.settings.modRole });
+        const roleAdm = guild.roles.find(r => { return r.name == message.settings.adminRole });
 
+        // Les commandes de la catégorie "Modérations" ne sont autorisé que pour les modérateurs et admins
         if (command.category.id == "Modérations") {
             if (member.roles.has(roleMod.id) || member.roles.has(roleAdm.id)) {
                 return false;
