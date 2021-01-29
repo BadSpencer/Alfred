b401a945e3226c01d49c6cb4418ec598193e783d
BadSpencer <badoffset@gmail.com>
2021-01-29 22:41:47 +0100

    Amélioration

--- commands/Modération/Members/note.js
+++ commands/Modération/Members/note.js
@@ -10,48 +10,44 @@ const {
     promptMessage
 } = require('../../../utils/messages');
 const colors = require('../../../utils/colors');
+const textes = new (require(`../../../utils/textes.js`));
 
 class NoteCommand extends Command {
     constructor() {
-        super('note', {
-            aliases: ['note'],
-            category: 'Modération',
-            description: 'Ajouter une note sur un membre',
-            args: [
-                {
-                    id: 'userdata',
-                    type: 'userdata',
-                    prompt: {
-                        start: message => promptMessage('Pour quel membre voulez vous ajouter une note ?'),
-                        retry: message => promptMessage('Mentionnez un membre avec son ID'),
-                    },
-                },
-                {
-                    id: 'note',
-                    type: "content",
-                    match: "rest",
-                    prompt: {
-                        start: message => promptMessage('Quelle est votre note ?'),
-                    },
-                }
-            ],
+        super("note", {
+            aliases: ["note"],
+            category: "Modération",
             description: {
-                content: 'Ajouter une note sur un membre',
-                usage: '',
+                content: textes.get("USER_NOTE_DESCRIPTION_CONTENT"),
+                usage: textes.get("USER_NOTE_DESCRIPTION_USAGE"),
                 examples: ['']
-              }
+            }
         });
     }
 
+    async *args(message) {
+        const member = yield {
+            type: "member",
+            prompt: {
+                start: message => promptMessage(textes.get('USER_NOTE_MEMBER_PROMPT'))
+            }
+        };
+
+        const note = yield {
+            type: "string",
+            prompt: {
+                start: message => promptMessage(textes.get("USER_NOTE_NOTE_PROMPT", member))
+            }
+        };
+        return {
+            member,
+            note
+        };
+    }
+
     async exec(message, args) {
-        let client = this.client;
-        const guild = client.getGuild();
-        let member = guild.members.cache.get(args.userdata.id);
-
-        if (!member) return errorMessage(client.textes.get("USER_ERROR_NOT_A_MEMBER", args.userdata.displayName), message.channel);
-
-        client.memberLogNote(member.id, message.author.id, args.note);
-        client.modLog(client.textes.get("MOD_NOTIF_MEMBER_NEW_NOTE", member, message.member, args.note));
+        this.client.memberLogNote(args.member.id, message.author.id, args.note);
+        this.client.modLog(this.client.textes.get("MOD_NOTIF_MEMBER_NEW_NOTE", args.member, message.member, args.note));
     }
 }
 
