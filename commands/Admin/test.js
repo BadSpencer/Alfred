2b41acbc692e322912d8a0b362582a110f899990
BadSpencer <badoffset@gmail.com>
2021-06-02 06:22:39 +0000

    test

--- commands/Admin/test.js
+++ commands/Admin/test.js
@@ -3,6 +3,14 @@ const {
 } = require("discord-akairo");
 const { Permissions } = require("discord.js");
 const steamServerStatus = require('steam-server-status');
+const {
+    successMessage,
+    errorMessage,
+    warnMessage,
+    questionMessage,
+    promptMessage
+} = require('../../utils/messages');
+const runner = require("child_process");
 
 class TestCommand extends Command {
     constructor() {
@@ -15,13 +23,33 @@ class TestCommand extends Command {
         });
     }
 
-    async exec(message) {
+    async *args(message) {
+        const userdata = yield {
+            type: "userdata",
+            prompt: {
+                start: message => promptMessage(`Spécifier un membre`),
+                retry: message => promptMessage(textes.get('USER_NOTEADD_MEMBER_RETRY'))
+            }
+        };
+
+        return {
+            userdata
+        };
+
+    }
+
+    async exec(message, args) {
         let client = this.client;
 
-        client.messageOfTheDay();
-        //client.gamesInfosPost();
+        // var phpScriptPath = "/var/www/joomla/user.php";
+        // // var argsString = "value1,value2,value3";
+        // const argsString = `${args.userdata.displayName},${args.userdata.id},${args.userdata.displayName}@casual-effect.fr`;
+        // runner.exec("php " + phpScriptPath + " " +argsString, function(err, phpResponse, stderr) {
+        //  if(err) console.log(err); /* log error */
+        // console.log( phpResponse );
+        // });
 
-        if (message.channel.type === 'text') message.delete();;
+        if (message.channel.type === 'text') message.delete();
     }
 }
 
